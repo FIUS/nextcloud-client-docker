0e43d61fee914fb4a45d91d930e5a5d692cae019
Julian Siebert <34137096+colfes@users.noreply.github.com>
2022-11-15 14:31:32 +0100

    fix(sync): Set verbosity to slient
    Normal verbosity did 200GB logs in 3 days

--- nextcloud-sync-loop.sh
+++ nextcloud-sync-loop.sh
@@ -2,7 +2,7 @@
 while :
 do
   # nc sync command here
-  nextcloudcmd --non-interactive -u $NC_USER -p $NC_PASS /sync/data $NC_URL
+  nextcloudcmd --non-interactive --silent -u $NC_USER -p $NC_PASS /sync/data $NC_URL
   #sync every 3*60=180 seconds
   sleep $SLEEP_INTERVAL
 done
